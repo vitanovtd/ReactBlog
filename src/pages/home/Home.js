56cf472a1125276b3f7e7ed784d8a8a4015c522e
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2022-08-29 20:29:35 +0300

    Added Sidebar and Home

---
+++ src/pages/home/Home.js
@@ -0,0 +1,19 @@
+import Header from "../../header/Header"
+import "./home.css"
+import Sidebar from "../../sidebar/Sidebar";
+import Posts from "../../posts/Posts";
+export default function Home() {
+    return (
+        <>
+            <Header />
+
+            <div className="home">
+                <Posts />
+                <Sidebar />
+                homePage
+            </div>
+        </>
+
+
+    )
+}
---
+++ src/posts/Posts.js
@@ -0,0 +1,11 @@
+import "./posts.css"
+
+
+export default function Posts() {
+    return (
+
+        <div className="posts">
+            Posts
+        </div>
+    )
+}
