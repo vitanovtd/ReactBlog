3216a05e44cfab43034f12720acdd4dcf4b2102d
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2022-09-12 23:14:40 +0300

    updated Routes

--- src/pages/home/Homepage.js
+++ src/pages/home/Homepage.js
@@ -2,7 +2,7 @@ import Header from "../../components/header/Header"
 import "./home.css"
 import Sidebar from "../../components/sidebar/Sidebar";
 import Posts from "../../components/posts/Posts";
-export default function Home() {
+export default function Homepage() {
     return (
         <>
             <Header />
